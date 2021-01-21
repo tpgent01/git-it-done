65ad33c756c6a77e6c6708f1c6b7d902568f4146
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-21 13:05:12 -0600

    convert fetch data into DOM element

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -5,7 +7,8 @@ var getRepoIssues = function(repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                // pass response data to dom function
+                displayIssues(data);
             });
         }
         else {
@@ -13,4 +16,41 @@ var getRepoIssues = function(repo) {
         }
     });
 };
+
+var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+        typeEl.textContent = "(Pull request)";
+        } else {
+        typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
 getRepoIssues("facebook/react");

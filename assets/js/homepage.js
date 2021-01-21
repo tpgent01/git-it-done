c1b17a3854a5d6c7f74874e0c69c5dacc07b2c51
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-20 21:16:28 -0600

    display repo data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -32,8 +32,29 @@ var getUserRepos = function(user) {
 
 
 var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 };
 
 
